3a4dba5d5acacd6e4c9ae674573914c1e14c6fbd
Artyom <artyom@MacBook-Pro-0000.local>
2026-05-30 12:38:43 +0300

    fix: enum from orderSchemas.js

--- schemas/orderSchemas.js
+++ schemas/orderSchemas.js
@@ -2,5 +2,11 @@ import { z } from 'zod'
 
 export const updateOrderStatusSchema = z.object({
     status: z
-        .enum(['pending', 'paid', 'shipped', 'completed', 'cancelled'])
+        .enum([
+            'pending',
+            'processing',
+            'shipped',
+            'delivered',
+            'cancelled'
+        ])
 })
